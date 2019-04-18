0d8deb395481d22cb5ecec1ee05e5a337a9485db
Christoph Schnackenberg <cschnack@gmx.de>
2019-04-18 11:56:16 +0200

    - extracted the EaseFuncPropertyParser to a static function so it can be used outside the property parser context

--- src/ttjs/entity/parser/EaseFuncPropertyParser.js
+++ src/ttjs/entity/parser/EaseFuncPropertyParser.js
@@ -32,6 +32,88 @@ define([
     function EaseFuncPropertyParser() {    
     }
 
+    /**
+     * This function does the actual transform.
+     *
+     * It returns the matching ease function. If null is returned check for EaseFuncPropertyParser._issue to determine
+     * the reason.
+     *
+     * @static
+     */
+    EaseFuncPropertyParser.toEaseFunc = (easeFuncDef) => {
+
+        EaseFuncPropertyParser._issue = null;
+
+        let easeName = "";
+        let easeMode = "inout";
+
+
+        // We accept string like this: Sine.in
+        if (_.isString(easeFuncDef)) {
+            const splitPos = easeFuncDef.indexOf(".");
+            if (splitPos == -1) { // no '.'
+                easeName = easeFuncDef;
+            }
+            else {
+                const parts = easeFuncDef.split(".");
+                easeName = parts[0];
+                easeMode = parts[1];
+            }
+        }
+        // we also accept an array with two values ["sine", "in"]
+        else if (_.isArray(easeFuncDef) && easeFuncDef.length == 2) {
+            easeName = easeFuncDef[0];
+            easeMode = easeFuncDef[1];
+        }
+        else {
+            EaseFuncPropertyParser._issue = "Unknown type for ease-func. Provide a string or an array with two values.";
+            return null;
+        }
+
+
+        // to avoid (very slow) toLowerCase we simply do some
+        // switching here.
+
+        let easeType = null;
+        switch(easeName) {
+            case "back": case "Back": easeType = Easing.Back; break;
+            case "bounce": case "Bounce": easeType = Easing.Bounce; break;
+            case "circular": case "Circular": easeType = Easing.Circular; break;
+            case "cubic": case "Cubic": easeType = Easing.Cubic; break;
+            case "elastic": case "Elastic": easeType = Easing.Elastic; break;
+            case "exponential": case "Exponential": easeType = Easing.Exponential; break;
+            case "linear": case "Linear": easeType = Easing.Linear; break;
+            case "quadratic": case "Quadratic": easeType = Easing.Quadratic; break;
+            case "quartic": case "Quartic": easeType = Easing.Quartic; break;
+            case "quintic": case "Quintic": easeType = Easing.Quintic; break;
+            case "sine": case "Sine": easeType = Easing.Sine; break;
+        }
+        if (!easeType) {
+            EaseFuncPropertyParser._issue =  "Unknown ease type: '" + easeName + "'";
+            return null;
+        }
+
+
+        let finalFunc = null;
+        switch(easeMode) {
+            case "in": case "In": case "easeIn": case "EaseIn":
+                finalFunc = easeType["easeIn"];
+                break;
+            case "out": case "Out": case "easeOut": case "EaseOut":
+                finalFunc = easeType["easeOut"];
+                break;
+            case "inout": case "inOut": case "easeInOut": case "EaseInOut":
+                finalFunc = easeType["easeInOut"];
+                break;
+        }
+
+        if (!easeType) {
+            EaseFuncPropertyParser._issue =  "For ease type: '" + easeName + "' unknown func: '" + easeMode + "'";
+            return null;
+        }
+
+        return finalFunc;
+    };
 
     EaseFuncPropertyParser.prototype = {
                 
@@ -45,68 +127,10 @@ define([
          */
         parse: function(propertyName, propertyInfo, instanceValue, outProps)
         {
-
-            let easeName = "";
-            let easeMode = "inout";
-
-
-            // We accept string like this: Sine.in
-            if (_.isString(instanceValue)) {
-                const splitPos = instanceValue.indexOf(".");
-                if (splitPos == -1) { // no '.'
-                    easeName = instanceValue;
-                }
-                else {
-                    const parts = instanceValue.split(".");
-                    easeName = parts[0];
-                    easeMode = parts[1];
-                }
+            const finalFunc = EaseFuncPropertyParser.toEaseFunc(instanceValue);
+            if (finalFunc == null) {
+                return EaseFuncPropertyParser._issue;
             }
-            // we also accept an array with two values ["sine", "in"]
-            else if (_.isArray(instanceValue) && instanceValue.length == 2) {
-                easeName = instanceValue[0];
-                easeMode = instanceValue[1];
-            }
-            else {
-                return "Unknown type for ease-func. Provide a string or an array with two values.";
-            }
-
-
-            // to avoid (very slow) toLowerCase we simply do some
-            // switching here.
-
-            let easeType = null;
-            switch(easeName) {
-                case "back": case "Back": easeType = Easing.Back; break;
-                case "bounce": case "Bounce": easeType = Easing.Bounce; break;
-                case "circular": case "Circular": easeType = Easing.Circular; break;
-                case "cubic": case "Cubic": easeType = Easing.Cubic; break;
-                case "elastic": case "Elastic": easeType = Easing.Elastic; break;
-                case "exponential": case "Exponential": easeType = Easing.Exponential; break;
-                case "linear": case "Linear": easeType = Easing.Linear; break;
-                case "quadratic": case "Quadratic": easeType = Easing.Quadratic; break;
-                case "quartic": case "Quartic": easeType = Easing.Quartic; break;
-                case "quintic": case "Quintic": easeType = Easing.Quintic; break;
-                case "sine": case "Sine": easeType = Easing.Sine; break;
-            }
-            if (!easeType)
-                return "Unknown ease type: '" + easeName + "'";
-
-            let finalFunc = null;
-            switch(easeMode) {
-                case "in": case "In": case "easeIn": case "EaseIn":
-                    finalFunc = easeType["easeIn"];
-                    break;
-                case "out": case "Out": case "easeOut": case "EaseOut":
-                    finalFunc = easeType["easeOut"];
-                    break;
-                case "inout": case "inOut": case "easeInOut": case "EaseInOut":
-                    finalFunc = easeType["easeInOut"];
-                    break;
-            }
-
-            if (!easeType)
-                return "For ease type: '" + easeName + "' unknown func: '" + easeMode + "'";
 
             outProps[propertyName] = finalFunc;
         }        
