187f5deb0ac79d4c23ffa79cfb2a69fae35906b6
Christoph Schnackenberg <cschnack@gmx.de>
2019-04-19 17:48:12 +0200

    adjusted ScaleBitmap to scale the edges when the DrawSize gets small

--- src/ttjs/lib/ScaleBitmap.js
+++ src/ttjs/lib/ScaleBitmap.js
@@ -31,7 +31,8 @@
  *
  * Copyright (c) 2019, Christoph Schnackenberg <bluechs@gmx.de>
  *
- * Adjusted it to work as AMD thingy
+ * - Adjusted it to work as AMD thingy
+ * - changed the way it handles cases where the edges are thinner than the defined border
  */
 define([
 	'ttjs/lib/easeljs-1.0.0.min'
@@ -150,60 +151,166 @@ define([
 	p.draw = function(ctx, ignoreCache) {
 		if (this.DisplayObject_draw(ctx, ignoreCache)) { return true; }
 
-		var centerX = this.scale9Grid.width;
-		var centerY = this.scale9Grid.height;
-		if(centerX == 0) //vertical
+		var centerWidth = this.scale9Grid.width;
+		var centerHeight = this.scale9Grid.height;
+		if(centerWidth == 0) //vertical
 		{
-			if(centerY == 0)
+			if(centerHeight == 0)
 			{
 				throw "One of scale9Grid width or height must be greater than zero.";
 			}
 			var imageWidth = this.image.width;
 			var scale3Region1 = this.scale9Grid.y;
-			var scale3Region3 = this.image.height - scale3Region1 - centerY;
+			var scale3Region3 = this.image.height - scale3Region1 - centerHeight;
 			var oppositeEdgeScale = this.drawWidth / imageWidth;
 			var scaledFirstRegion = scale3Region1 * oppositeEdgeScale;
 			var scaledThirdRegion = scale3Region3 * oppositeEdgeScale;
 			var scaledSecondRegion = this.drawHeight - scaledFirstRegion - scaledThirdRegion;
 
 			ctx.drawImage(this.image, 0, 0, imageWidth, scale3Region1, 0, 0, this.drawWidth, scaledFirstRegion);
-			ctx.drawImage(this.image, 0, scale3Region1, imageWidth, centerY, 0, scaledFirstRegion, this.drawWidth, scaledSecondRegion);
-			ctx.drawImage(this.image, 0, scale3Region1 + centerY, imageWidth, scale3Region3, 0, scaledFirstRegion + scaledSecondRegion, this.drawWidth, scaledThirdRegion);
+			ctx.drawImage(this.image, 0, scale3Region1, imageWidth, centerHeight, 0, scaledFirstRegion, this.drawWidth, scaledSecondRegion);
+			ctx.drawImage(this.image, 0, scale3Region1 + centerHeight, imageWidth, scale3Region3, 0, scaledFirstRegion + scaledSecondRegion, this.drawWidth, scaledThirdRegion);
 		}
-		else if(centerY == 0) //horizontal
+		else if(centerHeight == 0) //horizontal
 		{
 			var imageHeight = this.image.height;
 			scale3Region1 = this.scale9Grid.x;
-			scale3Region3 = this.image.width - scale3Region1 - centerX;
+			scale3Region3 = this.image.width - scale3Region1 - centerWidth;
 			oppositeEdgeScale = this.drawHeight / this.image.height;
 			scaledFirstRegion = scale3Region1 * oppositeEdgeScale;
 			scaledThirdRegion = scale3Region3 * oppositeEdgeScale;
 			scaledSecondRegion = this.drawWidth - scaledFirstRegion - scaledThirdRegion;
 
 			ctx.drawImage(this.image, 0, 0, scale3Region1, imageHeight, 0, 0, scaledFirstRegion, this.drawHeight);
-			ctx.drawImage(this.image, scale3Region1, 0, centerX, imageHeight, scaledFirstRegion, 0, scaledSecondRegion, this.drawHeight);
-			ctx.drawImage(this.image, scale3Region1 + centerX, 0, scale3Region3, imageHeight, scaledFirstRegion + scaledSecondRegion, 0, scaledThirdRegion, this.drawHeight);
+			ctx.drawImage(this.image, scale3Region1, 0, centerWidth, imageHeight, scaledFirstRegion, 0, scaledSecondRegion, this.drawHeight);
+			ctx.drawImage(this.image, scale3Region1 + centerWidth, 0, scale3Region3, imageHeight, scaledFirstRegion + scaledSecondRegion, 0, scaledThirdRegion, this.drawHeight);
 		}
 		else
 		{
 			var left = this.scale9Grid.x;
 			var top = this.scale9Grid.y;
-			var right = this.image.width - centerX - left;
-			var bottom = this.image.height - centerY - top;
-			var scaledCenterX = this.drawWidth - left - right;
-			var scaledCenterY = this.drawHeight - top - bottom;
+			var right = this.image.width - centerWidth - left;
+			var bottom = this.image.height - centerHeight - top;
+			var scaledCenterWidth = this.drawWidth - left - right;
+			var scaledCenterHeight = this.drawHeight - top - bottom;
 
-			ctx.drawImage(this.image, 0, 0, left, top, 0, 0, left, top);
-			ctx.drawImage(this.image, left, 0, centerX, top, left, 0, scaledCenterX, top);
-			ctx.drawImage(this.image, left + centerX, 0, right, top, left + scaledCenterX, 0, right, top);
+			if (scaledCenterWidth > 0 && scaledCenterHeight > 0) {
 
-			ctx.drawImage(this.image, 0, top, left, centerY, 0, top, left, scaledCenterY);
-			ctx.drawImage(this.image, left, top, centerX, centerY, left, top, scaledCenterX, scaledCenterY);
-			ctx.drawImage(this.image, left + centerX, top, right, centerY, left + scaledCenterX, top, right, scaledCenterY);
+				// top left
+				ctx.drawImage(this.image, 0, 0, left, top, 0, 0, left, top);
+				// top middle
+				ctx.drawImage(this.image, left, 0, centerWidth, top, left, 0, scaledCenterWidth, top);
+				// top right
+				ctx.drawImage(this.image, left + centerWidth, 0, right, top, left + scaledCenterWidth, 0, right,  top);
 
-			ctx.drawImage(this.image, 0, top + centerY, left, bottom, 0, top + scaledCenterY, left, bottom);
-			ctx.drawImage(this.image, left, top + centerY, centerX, bottom, left, top + scaledCenterY, scaledCenterX, bottom);
-			ctx.drawImage(this.image, left + centerX, top + centerY, right, bottom, left + scaledCenterX, top + scaledCenterY, right, bottom);
+				// middle left
+				ctx.drawImage(this.image, 0, top, left, centerHeight, 0, top, left, scaledCenterHeight);
+				// middle middle
+				ctx.drawImage(this.image, left, top, centerWidth, centerHeight, left, top, scaledCenterWidth, scaledCenterHeight);
+				// middle right
+				ctx.drawImage(this.image, left + centerWidth, top, right, centerHeight, left + scaledCenterWidth, top, right, scaledCenterHeight);
+
+
+				// bottom left
+				ctx.drawImage(this.image, 0, top + centerHeight, left, bottom, 0, top + scaledCenterHeight, left, bottom);
+				// bottom
+				ctx.drawImage(this.image, left, top + centerHeight, centerWidth, bottom, left, top + scaledCenterHeight, scaledCenterWidth, bottom);
+				// bottom right
+				ctx.drawImage(this.image, left + centerWidth, top + centerHeight, right, bottom, left + scaledCenterWidth, top + scaledCenterHeight, right, bottom);
+			}
+			else if (scaledCenterHeight > 0) {
+
+				scaledCenterWidth = 0;
+				const xScale = (this.drawWidth) / (left + right);
+
+				// top left
+				ctx.drawImage(this.image,
+					0, 0, left, top,
+					0, 0, left*xScale, top);
+				// top right
+				ctx.drawImage(this.image,
+					left + centerWidth, 0, right, top,
+					(left + scaledCenterWidth)*xScale, 0, right * xScale,  top);
+
+				// middle left
+				ctx.drawImage(this.image,
+					0, top, left, centerHeight,
+					0, top, left*xScale, scaledCenterHeight);
+				// middle right
+				ctx.drawImage(this.image,
+					left + centerWidth, top, right, centerHeight,
+					(left + scaledCenterWidth) * xScale, top, right*xScale, scaledCenterHeight);
+
+				// bottom left
+				ctx.drawImage(this.image,
+					0, top + centerHeight, left, bottom,
+					0, top + scaledCenterHeight, left*xScale, bottom);
+				// bottom right
+				ctx.drawImage(this.image,
+					left + centerWidth, top + centerHeight, right, bottom,
+					(left + scaledCenterWidth)*xScale, top + scaledCenterHeight, right*xScale, bottom);
+
+			}
+			else if (scaledCenterWidth > 0) {
+
+				scaledCenterHeight = 0;
+				const yScale = (this.drawHeight) / (top+bottom);
+
+				// top left
+				ctx.drawImage(this.image,
+					0, 0, left, top,
+					0, 0, left, top*yScale);
+				// top middle
+				ctx.drawImage(this.image,
+					left, 0, centerWidth, top,
+					left, 0, scaledCenterWidth, top*yScale);
+				// top right
+				ctx.drawImage(this.image,
+					left + centerWidth, 0, right, top,
+					left + scaledCenterWidth, 0, right, top*yScale);
+
+				// bottom left
+				ctx.drawImage(this.image,
+					0, top + centerHeight, left, bottom,
+					0, (top + scaledCenterHeight) * yScale, left, bottom*yScale);
+				// bottom
+				ctx.drawImage(this.image,
+					left, top + centerHeight, centerWidth, bottom,
+					left, (top + scaledCenterHeight) * yScale, scaledCenterWidth, bottom*yScale);
+				// bottom right
+				ctx.drawImage(this.image,
+					left + centerWidth, top + centerHeight, right, bottom,
+					left + scaledCenterWidth, (top + scaledCenterHeight) * yScale, right, bottom*yScale);
+
+			}
+			else {
+
+				const xScale = (this.drawWidth) / (left + right);
+				const yScale = (this.drawHeight) / (top + bottom);
+
+				// top left
+				ctx.drawImage(this.image,
+					0, 0, left, top,
+					0, 0, left*xScale, top*yScale);
+
+				// top right
+				ctx.drawImage(this.image,
+					left + centerWidth, 0, right, top,
+					left * xScale, 0, right*xScale,  top*yScale);
+
+				// bottom left
+				ctx.drawImage(this.image,
+					0, top + centerHeight, left, bottom,
+					0, top*yScale, left*xScale, bottom*yScale);
+
+				// bottom right
+				ctx.drawImage(this.image,
+					left + centerWidth, top + centerHeight, right, bottom,
+					left*xScale, top*yScale, right*xScale, bottom*yScale);
+
+			}
+
+
 		}
 
 		return true;
